4a581a732e698be2932fabcbf548f2e5a14787f5
Andrew Culver <andrew.culver@gmail.com>
2012-03-25 14:59:44 -0400

    Visitors can add comments.

---
+++ app/controllers/bloggie/blog_comments_controller.rb
@@ -0,0 +1,25 @@
+class Bloggie::BlogCommentsController < Bloggie::ApplicationController
+  def create
+    
+    if params[:blog_post_id].match /$\d+^/
+      throw "For security reasons, we don't allow folks to look up blog posts by their primary key."
+    end
+    @post = BlogPost.find(params[:blog_post_id])
+    
+    # remove fields that shouldn't updatable in this context.
+    params[:blog_comment].delete :post_id
+    params[:blog_comment].delete :approved
+    params[:blog_comment].delete :created_at
+    params[:blog_comment].delete :updated_at
+        
+    @comment = @post.blog_comments.new(params[:blog_comment])
+
+    # let's just approve everything for now.
+    @comment.approved = true
+
+    if @comment.save
+      redirect_to @post
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bloggie::Engine.routes.draw do
   root :to => 'blog_posts#index'
-  resources :blog_posts, path: 'posts'
+  resources :blog_posts, path: 'posts' do
+    resources :blog_comments, path: 'comments'
+  end
   resources :blog_categories, path: 'categories'
 end
