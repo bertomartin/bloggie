857bec66b685afe9b694b1b4f8c8c14ffc12ac9e
Andrew Culver <andrew.culver@gmail.com>
2012-03-28 16:21:53 -0400

    Fixing redirect for B&B.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  scope "/blog", module: 'Bloggie' do
+
+  match "/blog", :to => redirect("/charlottesville/blog")
+  match "/blog/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/blog/%{id}")
+
+  scope "/charlottesville/blog", module: 'Bloggie' do
     root :to => 'blog_posts#index', as: 'blog'
     resources :blog_posts, path: 'posts' do
       resources :blog_comments, path: 'comments'
