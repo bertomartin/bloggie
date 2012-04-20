57a38af651cd1a419feecce3b672d7039cd129e3
Andrew Culver <andrew.culver@gmail.com>
2012-04-20 14:52:06 -0400

    Removing 'posts' from the URL for blog posts.

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
   match "/charlottesville/blog", :to => redirect("/charlottesville/wedding-blog")
   match "/charlottesville/blog/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/wedding-blog/%{id}")
 
+  match "/charlottesville/wedding-blog/posts/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/wedding-blog/%{id}")
+
   scope "/charlottesville/wedding-blog", module: 'Bloggie' do
     root :to => 'blog_posts#index', as: 'blog'
     resources :blog_posts, path: 'post' do
       resources :blog_comments, path: 'comments'
     end
     resources :blog_categories, path: 'categories'
+    get '/:id' => 'blog_posts#show', as: 'blog_post'
   end
   
 end
