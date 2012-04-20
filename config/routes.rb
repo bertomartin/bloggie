325ba889f15e12214a9dc2e17c99effb32931799
Andrew Culver <andrew.culver@gmail.com>
2012-04-20 14:35:35 -0400

    Adjusting paths for B&B.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  match "/blog", :to => redirect("/charlottesville/blog")
-  match "/blog/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/blog/%{id}")
+  match "/blog", :to => redirect("/charlottesville/wedding-blog")
+  match "/blog/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/wedding-blog/%{id}")
 
-  scope "/charlottesville/blog", module: 'Bloggie' do
+  match "/charlottesville/blog", :to => redirect("/charlottesville/wedding-blog")
+  match "/charlottesville/blog/:id", :constraints => {:id => /.+/}, :to => redirect("/charlottesville/wedding-blog/%{id}")
+
+  scope "/charlottesville/wedding-blog", module: 'Bloggie' do
     root :to => 'blog_posts#index', as: 'blog'
-    resources :blog_posts, path: 'posts' do
+    resources :blog_posts, path: 'post' do
       resources :blog_comments, path: 'comments'
     end
     resources :blog_categories, path: 'categories'
   end
+  
 end
